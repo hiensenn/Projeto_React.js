03caafe8a4b9e4520cff2b242e51f53d40f7582b
HiensenLima <hiensen.lima@sienacompany.com>
2022-11-21 18:57:14 -0300

    Props e Componentização

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+
+import './styles.css';
+
+import { Posts } from '../../components/Posts';
+import { loadPosts } from '../../utils/load-posts';
+
+export class Home extends Component {
+  state = {
+    posts: [],
+    allPosts: [],
+  };
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+    this.setState({
+       posts: postsAndPhotos,
+      allPosts: postsAndPhotos,
+      });
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <section className="container">
+        <Posts posts={posts} />
+      </section>
+    );
+  }
+}
