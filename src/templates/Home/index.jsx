7bc438f4b7ffee7130b8fa1aebb98474e8848f61
HiensenLima <hiensen.lima@sienacompany.com>
2023-01-05 00:14:02 -0300

    finalizando paginação

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -4,13 +4,16 @@ import './styles.css';
 
 import { Posts } from '../../components/Posts';
 import { loadPosts } from '../../utils/load-posts';
+import { Button } from '../../components/Button';
+
+
 
 export class Home extends Component {
   state = {
     posts: [],
     allPosts: [],
     page: 0,
-    pagesPerPage: 2,
+    postsPerPage: 14,
   };
 
   async componentDidMount() {
@@ -18,20 +21,44 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {
-    const {page, pagesPerPage} = this.state;
+    const {page, postsPerPage} = this.state;
     const postsAndPhotos = await loadPosts();
     this.setState({
-      posts: postsAndPhotos.slice(page, pagesPerPage),
+      posts: postsAndPhotos.slice(page, postsPerPage),
       allPosts: postsAndPhotos,
       });
   }
 
+  loadMorePosts = () => {
+    const{
+      page,
+      postsPerPage,
+      allPosts,
+      posts
+    } = this.state;
+
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage});
+  }
+
   render() {
-    const { posts } = this.state;
+    const { posts, page, postsPerPage, allPosts} = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
 
     return (
       <section className="container">
         <Posts posts={posts} />
+        <div class='button-container'>
+        <Button 
+        text="load more posts"
+        onClick={this.loadMorePosts}
+        disabled={noMorePosts}
+        />
+        </div>
+        
       </section>
     );
   }
